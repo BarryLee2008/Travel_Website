8c7e337cf1c151c23f424308f69ee4dd66bb07d4
BarryLee <barryalan@163.com>
2022-08-26 20:28:03 -0700

    new App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import styles from './App.module.css';
 
 // 引入Footer和Header组件.这里{}中的名字要和声明的函数组件名一样
-import {Header,Footer} from "./components"
+// 这个地方是将components变成一个模块
+import {Header,Footer} from "./components" 
 
 function App() {
   return (
